9f29457db95e3907e7c84244cfd570fc8c4269d5
2017tmxkxm <2017tmxkxm@gmail.com>
2024-04-23 15:26:48 +0900

    [Update] id, answerList 추가

--- src/main/java/com/mysite/csJpa/question/dto/QuestionViewResponse.java
+++ src/main/java/com/mysite/csJpa/question/dto/QuestionViewResponse.java
@@ -1,16 +1,23 @@
 package com.mysite.csJpa.question.dto;
 
+import com.mysite.csJpa.answer.Answer;
 import com.mysite.csJpa.question.Question;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class QuestionViewResponse {
 
+    private final int id;
     private final String subject;
     private final String content;
+    private final List<Answer> answerList;
 
     public QuestionViewResponse(Question question) {
+        this.id = question.getId();
         this.subject = question.getSubject();
         this.content = question.getContent();
+        this.answerList = question.getAnswerList();
     }
 }
