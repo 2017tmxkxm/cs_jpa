21e5ed1d388b058776bdb59273710ec9ce2ba3b0
2017tmxkxm <2017tmxkxm@gmail.com>
2024-06-11 16:07:19 +0900

    [Create] CommentRepository

---
+++ src/main/java/com/mysite/csJpa/comment/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.mysite.csJpa.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findByQuestionId(int questionId);
+}
